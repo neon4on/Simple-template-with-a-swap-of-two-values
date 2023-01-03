5c27f8d7cb3dfc64faffc8a8344f271a9b1c7f66
Valeriy Kamenskikh <86181396+neon4on@users.noreply.github.com>
2023-01-03 16:03:14 +0400

    Add files via upload

---
+++ 1/1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+template<typename T>
+void Swap(T& a, T& b)
+{
+	T temp = a;
+	a = b;
+	b = temp;
+}
+
+void main()
+{
+	setlocale(LC_ALL, "ru");
+	string var1 = "Привет!";
+	string var2 = "Пока!";
+
+	cout << "Normal:" << endl;
+
+	cout << "var1\t" << var1 << endl;
+	cout << "var2\t" << var2 << endl << endl;
+
+	cout << "Swap:" << endl;
+
+	Swap(var1, var2);
+
+	cout << "var1\t" << var1 << endl;
+	cout << "var2\t" << var2 << endl;
+}
